f8a81fb973be08fa1c5f710dd69d4af96619f0df
Ali Bayatpoor <alizbay94@gmail.com>
2020-11-05 16:41:02 -0500

    updates to cards and deck

--- main.c
+++ main.c
@@ -16,13 +16,18 @@ typedef struct card Card;
 
 //===================== Prototypes / Globals =======================//
 void populate_deck(Card []);
-void display_cards(const Card deck[]);
+void display_cards_faceUp(const Card deck[]);
+void display_welcome_message();
 
 // array of pointers to characters for symbols
 char *symbols[MAX_SYMBOLS] = {"!", "@", "#", "$", "^", "&", "*", "+", "x"};
 
+
+
 //===================== Main Begins... =======================//
 int main () {
+
+    display_welcome_message();
 
     // "deck" is an array of 18 cards
     Card deck[MAX_CARDS];
@@ -30,11 +35,13 @@ int main () {
     populate_deck(deck);
 
     printf("Printing unshuffled deck:\n");
-    display_cards(deck);
+    display_cards_faceUp(deck);
     return 0;
 }
 
-// populate_decks deck of cards
+
+//===================== Functions / Methods =======================//
+// fills a deck with cards
 void populate_deck(Card deck[]){
   int i = 0;
   for(i=0;i<MAX_CARDS;i++){
@@ -43,11 +50,24 @@ void populate_deck(Card deck[]){
 }
 
 // Prints deck of cards
-void display_cards(const Card deck[]){
+void display_cards_faceUp(const Card deck[]){
   int i = 0;
   for(i=0;i<MAX_CARDS;i++){
-    printf("%s", deck[i].symbol);
-    printf("\n");
+    printf("[%s] ", deck[i].symbol);
+    
+    if((i == 5) || (i == 11)){
+        printf("\n");
     }
   }
+  printf("\n\n");
+}
+
+void display_welcome_message(){
+    printf("\n");
+    printf("    +------------------------------+\n");
+    printf("    |                              |\n");
+    printf("    |      MATCHING CARD GAME      |\n");
+    printf("    |                              |\n");
+    printf("    +------------------------------+\n\n");
+}
 
