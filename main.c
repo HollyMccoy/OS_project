ff6ba19b4c87b9c092906dfab7397f7db755b978
Ali Bayatpoor <alizbay94@gmail.com>
2020-11-05 17:02:35 -0500

    added shuffle feature

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 //===================== Cards =======================//
 //structure definition
 struct card{ 
-  char *symbol;    
+  char *symbol; // this is the special symbol
 };
 typedef struct card Card;
 
@@ -18,6 +18,8 @@ typedef struct card Card;
 void populate_deck(Card []);
 void display_cards_faceUp(const Card deck[]);
 void display_welcome_message();
+void shuffle_deck();
+int get_random_num(int numBeg, int numEnd);
 
 // array of pointers to characters for symbols
 char *symbols[MAX_SYMBOLS] = {"!", "@", "#", "$", "^", "&", "*", "+", "x"};
@@ -29,12 +31,19 @@ int main () {
 
     display_welcome_message();
 
+    srand(time(0)); 
     // "deck" is an array of 18 cards
     Card deck[MAX_CARDS];
 
     populate_deck(deck);
 
     printf("Printing unshuffled deck:\n");
+    display_cards_faceUp(deck);
+
+    printf("Shuffling deck\n");
+    shuffle_deck(deck);
+
+    printf("Printing shuffled deck:\n");
     display_cards_faceUp(deck);
     return 0;
 }
@@ -60,6 +69,27 @@ void display_cards_faceUp(const Card deck[]){
     }
   }
   printf("\n\n");
+}
+
+// Shuffles cards using Fisher Yates Shuffle Algorithm
+void shuffle_deck(Card deck[]) {
+    int i, j;
+    Card temp;
+    for (i = 0; i < MAX_CARDS - 1; ++i) {
+        j = i + (get_random_num(0, MAX_CARDS - i)); 
+        //swapping
+        temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+
+    }
+}
+
+
+// Returns a random number according to specifications
+int get_random_num(int numBeg, int numEnd) {
+    int ranNum = rand() % numEnd + numBeg;
+    return ranNum;
 }
 
 void display_welcome_message(){
