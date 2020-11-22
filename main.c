ab34ea0193714f432ae88c23e200b36dd1a9102e
Holly <55715908+HollyMccoy@users.noreply.github.com>
2020-11-22 16:17:33 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 
 #define MAX_CARDS 18
 #define MAX_SYMBOLS 9
-#define TEST_MODE 1 // If TEST_MODE = 1 (True) Will Begin Testing TEST_MODE = 0 (False) Will Skip Testing
+#define MAX_BUFFER 256
+#define MAX_PLAYERS 5
+#define TEST_MODE 0 // If TEST_MODE = 1 (True) Will Begin Testing TEST_MODE = 0 (False) Will Skip Testing
 
 //===================== Cards =======================//
 //structure definition
@@ -18,9 +20,11 @@ typedef struct card Card;
 
 //===================== Prototypes / Globals =======================//
 void populate_deck(Card []);
-void print_deck_faceup( Card deck[]);
-void print_deck();
-void display_welcome_message();
+void print_deck_faceup( Card deck[]); //Replaced by char * faceup_deck_to_buffer
+char * faceup_deck_to_buffer(Card deck[], char buffer[]);
+void print_deck(); //Replaced by char * facedown_deck_to_buffer
+char * facedown_deck_to_buffer(const Card *deck, char buffer[]);
+void display_welcome_message(); //Replaced in main by printf statement
 void shuffle_deck();
 int get_random_num(int numBeg, int numEnd);
 int char_to_num_convert(char theChar);
@@ -44,16 +48,27 @@ int main () {
 
 
     } else {
-    display_welcome_message();
+    //Game welcome message
+    char * welcome = "\n"
+    "    +------------------------------+\n"
+    "    |                              |\n"
+    "    |      MATCHING CARD GAME      |\n"
+    "    |                              |\n"
+    "    +------------------------------+\n\n";
+    printf(welcome);
 
     srand(time(0)); 
     // "deck" is a 'struct card' or 'Card' type array
     Card deck[MAX_CARDS];
 
+    //"buffer" is an array containing messages to be sent to clients
+    char buffer[MAX_BUFFER];
+
     //filling deck with cards
     populate_deck(deck);
     shuffle_deck(deck);
-    print_deck_faceup(deck);
+    //print_deck_faceup(deck);
+    printf(faceup_deck_to_buffer(deck, buffer));
 
     //printf("Printing unshuffled deck:\n");
     //print_deck_faceup(deck);
@@ -68,22 +83,21 @@ int main () {
     //print_deck(deck);
 
     char input;
-    int player1_score = 0;
-    int player2_score = 0;
     int cardLocation, cardLocation2;
     Card firstCard, secondCard; //these will be compared
-    bool player1_turn = false;
-    bool player2_turn = false;
     bool stillPlaying = true;
 
     bool isTakeTurns = true;
+    int playerTurn = 0; //Tracks which player's turn it is; starts at 0
+    int numOfPlayers = 2;
+    int playerScores[MAX_PLAYERS] = {0}; //Tracks each player's score
 
     //------------------------------- Game Loop Begins ------------------------------------
     // this game mode is for when the players take turns
     if (isTakeTurns){
-        player1_turn = true; // since they are taking turns player 1 goes first
         while(stillPlaying){
-            print_deck(deck);
+            //print_deck(deck);
+            printf(facedown_deck_to_buffer(deck, buffer));
             
             fflush(stdin);
               // userSelection = 'b';
@@ -109,7 +123,8 @@ int main () {
               // flipping the card face up
               deck[cardLocation].isFlipped = true;
 
-              print_deck(deck);
+              //print_deck(deck);
+              printf(facedown_deck_to_buffer(deck, buffer));
               
               fflush(stdin);
               // userSelection = 'b';
@@ -136,29 +151,23 @@ int main () {
               deck[cardLocation2].isFlipped = true;
 
               //reveal card on board
-              print_deck(deck);
+              //print_deck(deck);
+              printf(facedown_deck_to_buffer(deck, buffer));
             
-
               //if both card's symbols match
               if (deck[cardLocation].symbol == deck[cardLocation2].symbol){
                 printf("Match!\n");
                 //taking the cards out of play
                 deck[cardLocation].inPlay = false;
                 deck[cardLocation2].inPlay = false;
-                if(player1_turn){
-                  player1_score+=1;
-                  player1_turn = false;
-                  player2_turn = true;
-                }
-                else if(player2_turn){
-                  player2_score+=1;
-                  player2_turn = false;
-                  player1_turn = true;
-                }
+
+                //Adding point to player
+                playerScores[playerTurn]++;
+
                 // function checks to see if all cards are face up, if so game over
                 if(isGameOver(deck)){
                   printf("\n\nGame Over\n\n");
-                  printf("Final Scores\n\nPlayer 1: %d\nPlayer2: %d\n\n", player1_score, player2_score);
+                  printf("Final Scores\n\nPlayer 1: %d\nPlayer 2: %d\n\n", playerScores[0], playerScores[1]);
                   break;
                 }
                 
@@ -169,7 +178,11 @@ int main () {
                 deck[cardLocation].isFlipped = false;  
                 deck[cardLocation2].isFlipped = false;
               }
-              printf("Scores\n\nPlayer 1: %d\nPlayer2: %d\n\n", player1_score, player2_score);
+              printf("Scores\n\nPlayer 1: %d\nPlayer 2: %d\n\n", playerScores[0], playerScores[1]);
+
+              //Rotate player turn
+              if(++playerTurn == numOfPlayers)
+                playerTurn = 0;
           }
     }
 
@@ -211,9 +224,44 @@ void print_deck(const Card deck[]){
   printf("\n\n");
 }
 
+//Populates buffer deck of cards symbol-side down, unless the card's isFlipped
+//is true; returns pointer to buffer for use with print statements and message sending
+char * facedown_deck_to_buffer(const Card *deck, char buffer[]){
+    //To-do: add error checks for buffer size (more important for non-deck buffer functions)
+    int j = 0;
+    int i;
+    char letter = 97; //Using ASCII to print alphabet
+    buffer[j++] = '\t';
+    for( i=0; i<MAX_CARDS; i++, j++){
+        if(deck[i].isFlipped){
+            buffer[j++] = ' ';
+            buffer[j++] = *deck[i].symbol;
+            buffer[j++] = ' ';
+            buffer[j] = ' ';
+        }
+        else{
+            buffer[j++] = '[';
+            buffer[j++] = letter;
+            buffer[j++] = ']';
+            buffer[j] = ' ';
+        }
+        letter++;
+
+        if((i == 5) || (i == 11)){
+            buffer[j++] = '\n';
+            buffer[j] = '\t';
+        }
+    }
+    buffer[j++] = '\n';
+    buffer[j++] = '\n';
+    buffer[j] = '\0';
+    return buffer;
+}
+
 void print_deck_faceup( Card deck[]){
+    int i;
   printf("Solution:\n               ");
-  for(int i=0;i<MAX_CARDS;i++){
+  for(i=0;i<MAX_CARDS;i++){
     printf("[%s] ", deck[i].symbol);
     
     if((i == 5) || (i == 11)){
@@ -222,6 +270,29 @@ void print_deck_faceup( Card deck[]){
   }
   printf("\n\n");
 } 
+
+//Populates buffer with faceup deck; returns pointer to buffer for
+//use with print statements and message sending
+char * faceup_deck_to_buffer(Card deck[], char buffer[]){
+    //To-do: add error checks for buffer size (more important for non-deck buffer functions)
+    int i = 0,j;
+    buffer[i++] = '\t';
+    for(j=0;j<MAX_CARDS;j++, i++){
+        buffer[i++] = '[';
+        buffer[i++] = *deck[j].symbol;
+        buffer[i++] = ']';
+        buffer[i] = ' ';
+
+        if ((j == 5) || (j==11)){
+            buffer[i++] = '\n';
+            buffer[i] = '\t';
+        }
+    }
+    buffer[i++] = '\n';
+    buffer[i++] = '\n';
+    buffer[i] = '\0';
+    return buffer;
+}
 
 // Shuffles cards using Fisher Yates Shuffle Algorithm
 void shuffle_deck(Card deck[]) {
@@ -266,7 +337,8 @@ bool validate_input(char userInput, Card thedeck[MAX_CARDS]){
 // Iterates through deck, if there is a card still face down we return false,
 // if all cards are face up then we return true and the game is over
 bool isGameOver(Card deck[]){
-  for(int i=0;i<MAX_CARDS;++i){
+    int i;
+  for(i=0;i<MAX_CARDS;++i){
     if(deck[i].isFlipped == false){
       return false;
     }
